a20a085d8eda78e582156fbd6e41ef66448642c4
Ivan Popov <vanchello03@yandex.ru>
2025-10-19 00:44:43 +0300

    5

--- main.go
+++ main.go
@@ -2,59 +2,58 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"time"
 	"io"
-	"strings"
+	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
-	TrashLoadAvarage = 30
-	TrashPercentRamUsed = 0.8
+	TrashLoadAvarage     = 30
+	TrashPercentRAMUsed  = 0.8
 	TrashPercentDiskUsed = 0.9
-	TrashPercentNetUsed = 0.9
-	BytesInMB = 1048576
+	TrashPercentNetUsed  = 0.9
+	BytesInMB            = 1048576
 )
 
 var counter = 0
 
 type StatsServe struct {
 	LoadAvarage int
-	RAMTotal int
-	RAMUsed int
-	DiskTotal int
-	DiskUsed int
-	NetTotal int
-	NetUsed int
+	RAMTotal    int
+	RAMUsed     int
+	DiskTotal   int
+	DiskUsed    int
+	NetTotal    int
+	NetUsed     int
 }
 
 func (s *StatsServe) CheckStatistic() {
 
 	if s.LoadAvarage > TrashLoadAvarage {
-		fmt.Printf("Load Average is too high: %d\n",s.LoadAvarage)
+		fmt.Printf("Load Average is too high: %d\n", s.LoadAvarage)
 	}
 
-	if percentRAM:=(float32(s.RAMUsed)/float32(s.RAMTotal)); percentRAM > TrashPercentRamUsed {
+	if percentRAM := (float32(s.RAMUsed) / float32(s.RAMTotal)); percentRAM > TrashPercentRAMUsed {
 		fmt.Printf("Memory usage too high: %.0f%%\n", percentRAM*100)
 	}
 
-	if FreeDisk := -float32(s.DiskUsed)+float32(s.DiskTotal); FreeDisk/float32(s.DiskTotal) < (1-TrashPercentDiskUsed) {
+	if FreeDisk := -float32(s.DiskUsed) + float32(s.DiskTotal); FreeDisk/float32(s.DiskTotal) < (1 - TrashPercentDiskUsed) {
 		fmt.Printf("Free disk space is too low: %.0f Mb left\n", FreeDisk/BytesInMB)
 	}
 
-	if FreeNet := float32(s.NetTotal) - float32(s.NetUsed); FreeNet/float32(s.NetTotal) < (1-TrashPercentNetUsed) {
+	if FreeNet := float32(s.NetTotal) - float32(s.NetUsed); FreeNet/float32(s.NetTotal) < (1 - TrashPercentNetUsed) {
 		fmt.Printf("Network bandwidth usage high: %.0f Mbit/s available\n", FreeNet/BytesInMB)
 	}
 }
 
 func main() {
-	for i:=0; i<5; i++ {
-
+	for i := 0; i < 100; i++ {
 
 		response, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
 
-		if err!=nil {
+		if err != nil {
 			fmt.Println(err)
 			return
 		}
@@ -62,6 +61,8 @@ func main() {
 		if response.StatusCode != http.StatusOK {
 			counter++
 			continue
+		} else {
+			counter = 0
 		}
 
 		defer response.Body.Close()
@@ -76,19 +77,18 @@ func main() {
 		//body := "32,21474836481934,1073741824,5497558138880,4398046511104,104857600,6291456"
 		parts := strings.Split(string(body), ",")
 
-
-		var my_slice = make([]int, 7)
+		var mySlice = make([]int, 7)
 
 		for i, part := range parts {
 
 			value, err := strconv.Atoi(part)
 
-			if err != nil{
+			if err != nil {
 				fmt.Println(err)
 				counter++
 				break
 			}
-			my_slice[i] = value
+			mySlice[i] = value
 		}
 
 		if counter == 3 {
@@ -96,11 +96,10 @@ func main() {
 			return
 		}
 
-		Stats := StatsServe{my_slice[0], my_slice[1], my_slice[2], my_slice[3], my_slice[4], my_slice[5], my_slice[6]}
+		Stats := StatsServe{mySlice[0], mySlice[1], mySlice[2], mySlice[3], mySlice[4], mySlice[5], mySlice[6]}
 
 		//fmt.Println(Stats)
 		Stats.CheckStatistic()
-
 
 		time.Sleep(2 * time.Second)
 	}
